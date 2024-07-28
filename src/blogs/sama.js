5980ba7773d25620cfa385cf146c52d5d1d29118
TK <leandrotk100@gmail.com>
2024-07-28 10:30:57 -0300

    *

---
+++ src/blogs/sama.js
@@ -0,0 +1,7 @@
+document.getElementById('header').remove();
+document.getElementById('topbar').remove();
+document.getElementById('sidebar').remove();
+document.getElementById('flash').remove();
+document.getElementsByClassName('posthaven-control')[0].remove();
+document.getElementById('main').style = 'margin: 100px auto;';
+document.getElementsByTagName('h2')[0].style = 'font-size: 40px;';
