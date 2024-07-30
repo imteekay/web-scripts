740f2e1d1f12d327ee6ff3f5189f49302608aa0f
TK <leandrotk100@gmail.com>
2024-07-30 09:20:14 -0300

    *

--- src/blogs/sama.js
+++ src/blogs/sama.js
@@ -3,5 +3,5 @@ document.getElementById('topbar').remove();
 document.getElementById('sidebar').remove();
 document.getElementById('flash').remove();
 document.getElementsByClassName('posthaven-control')[0].remove();
-document.getElementById('main').style = 'margin: 100px auto;';
+document.getElementById('main').style = 'margin: 100px auto; max-width: 700px;';
 document.getElementsByTagName('h2')[0].style = 'font-size: 40px;';
