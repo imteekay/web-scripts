9b0a130f34a94a3e9286cef6d21d28f999f3ce65
TK <leandrotk100@gmail.com>
2024-07-30 12:04:30 -0300

    *

---
+++ src/blogs/hamming.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName('h1')[1].textContent = "You and Your Research";
+document.getElementsByTagName('center')[2].remove();
+document.getElementsByTagName('center')[2].remove();
+document.getElementsByTagName('body')[0].style = 'max-width: 700px; margin: 20px auto; font-size: 18px;';
